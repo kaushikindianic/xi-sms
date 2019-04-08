695921ce17610780e8f0c73a6ddb0602fae2315f
felixmaier1989 <felixmaier1989@gmail.com>
2019-04-08 10:36:35 +0700

    Twilio: Optionally set a Messaging Service

--- library/Xi/Sms/Gateway/TwilioGateway.php
+++ library/Xi/Sms/Gateway/TwilioGateway.php
@@ -27,33 +27,33 @@ class TwilioGateway extends BaseHttpRequestGateway
      */
     private $authToken;
 
-	/**
-	 * @var string
-	 */
-	private $numberFrom;
+    /**
+     * @var string
+     */
+    private $numberFrom;
 
-	/**
-	 * @var string|null
-	 */
-	private $messagingServiceId;
+    /**
+     * @var string|null
+     */
+    private $messagingServiceId;
 
-	/**
-	 * TwilioGateway constructor.
-	 * @param string $accountSid Your Account SID from twilio.com/console
-	 * @param string $authToken Your Auth Token from twilio.com/console
-	 * @param string $numberFrom A Twilio phone number you purchased at twilio.com/console
-	 * @param string|null $messagingServiceId
-	 */
+    /**
+     * TwilioGateway constructor.
+     * @param string $accountSid Your Account SID from twilio.com/console
+     * @param string $authToken Your Auth Token from twilio.com/console
+     * @param string $numberFrom A Twilio phone number you purchased at twilio.com/console
+     * @param string|null $messagingServiceId Messaging service SIDa
+     */
     public function __construct(
         $accountSid,
         $authToken,
         $numberFrom,
-		$messagingServiceId = null
+        $messagingServiceId = null
     ) {
         $this->accountSid = $accountSid;
         $this->authToken = $authToken;
-		$this->numberFrom = $numberFrom;
-		$this->messagingServiceId = $messagingServiceId;
+        $this->numberFrom = $numberFrom;
+        $this->messagingServiceId = $messagingServiceId;
     }
 
     /**
@@ -91,19 +91,19 @@ class TwilioGateway extends BaseHttpRequestGateway
         }
 
         $params = array(
-			'from' => '+' . $this->numberFrom,
-			// the body of the text message you'd like to send
-			'body' => $content,
-		);
+            'from' => '+' . $this->numberFrom,
+            // the body of the text message you'd like to send
+            'body' => $content,
+        );
         if ($this->messagingServiceId) {
-			$params['messagingServiceId'] = $this->messagingServiceId;
-		}
+            $params['messagingServiceSid'] = $this->messagingServiceId;
+        }
 
         // Use the client to do fun stuff like send text messages!
         $MessageInstance = $client->messages->create(
         // the number you'd like to send the message to
             '+' . $to,
-			$params
+            $params
         );
         $MessageInstance = $MessageInstance->toArray();
         if (empty($MessageInstance['sid'])) {
